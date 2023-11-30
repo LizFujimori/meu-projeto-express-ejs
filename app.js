11c171067025ff8c7f2a15441200100bdf293f06
LizFujimori <liizfdaan@gmail.com>
2023-11-30 13:39:29 -0400

    imagens

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 const produtos = [
-  {id: 1, nome: "Plushie Noelle", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://ae01.alicdn.com/kf/S77429093a5d74e058724f6df9a4e5002w.jpg_640x640Q90.jpg_.webp"},
-  {id: 2, nome: "Plushie Kakyoin", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://i.ebayimg.com/images/g/d~AAAOSwy2NfVbgO/s-l1600.jpg"},
-  {id: 3, nome: "Plushie Wriothesley", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://genshinbox.co/wp-content/uploads/2023/09/Genshin-Plushies-%E2%80%93-Wriothesley-Plush-Doll.jpg"},
-  {id: 4, nome: "Plushie Neuvillette", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://m.media-amazon.com/images/I/61ezhNzsFFL._AC_UF894,1000_QL80_.jpg"},
-  {id: 5, nome: "Plushie Joseph", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://jjba.shop/wp-content/uploads/2021/10/JoJos-Bizarre-Adventure-Tomonui-Plush-Doll-JOSEPH-JOESTAR-Battle-Tendency-new.jpg"},
-  {id: 6, nome: "Plushie Zhongli", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://i.ebayimg.com/images/g/SLIAAOSwIIFirUJj/s-l1600.jpg"},
-  {id: 7, nome: "Plushie Kazuha", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://toysonejapan.com/cdn/shop/products/s-l1600_22_8a7470e1-668c-48c2-91dc-7d60efc3108a_1024x1024.jpg?v=1675046966"},
-  {id: 8, nome: "Plushie Kars", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://i.ebayimg.com/images/g/Ex4AAOSwbMBiN21Q/s-l1200.webp"},
-  {id: 9, nome: "Plushie Barbados", preco: "R$ 6,99", deacricao: "plushie", imagem: "https://img.fruugo.com/product/5/53/942292535_max.jpg"}
+  {id: 1, nome: "Plushie Noelle", preco: "R$ 6,99", deacricao: "Plush - Noelle (Genshin Impact)", imagem: "noelle.jpg"},
+  {id: 2, nome: "Plushie Kakyoin", preco: "R$ 6,99", deacricao: "Plush - Noriaki Kakyoin (JoJo's Bizarre Adventure: Stardust Crusaders)", imagem: "kakyoin.jpg"},
+  {id: 3, nome: "Plushie Wriothesley", preco: "R$ 6,99", deacricao: "Plush - Wriothesley (Genshin Impact)", imagem: "rizzley.jpg"},
+  {id: 4, nome: "Plushie Neuvillette", preco: "R$ 6,99", deacricao: "Plush - Mounsier Neuvillette (Genshin Impact)", imagem: "neuvi.jpg"},
+  {id: 5, nome: "Plushie Joseph", preco: "R$ 6,99", deacricao: "Plush - Joseph Joestar (JoJo's Bizarre Adventure: Battle Tendency)", imagem: "joseph.jpg"},
+  {id: 6, nome: "Plushie Zhongli", preco: "R$ 6,99", deacricao: "Plush - Zhongli, Rex Lápis (Genshin Impact)", imagem: "zhongli.jpg"},
+  {id: 7, nome: "Plushie Kazuha", preco: "R$ 6,99", deacricao: "Plush - Kaedehara Kazuha (Genshin Impact)", imagem: "kazuha.jpg"},
+  {id: 8, nome: "Plushie Kars", preco: "R$ 6,99", deacricao: "Plush - Kars (JoJo's Bizarre Adventure: Battle Tendency)", imagem: "kars.jpg"},
+  {id: 9, nome: "Plushie Barbados", preco: "R$ 6,99", deacricao: "Plush - Barbados, Venti (Genshin Impact)", imagem: "barbados.jpg"}
 ]
 function buscarProdutoporID(){
   const porduto = produtos.find(produto => produto.id ==id);
